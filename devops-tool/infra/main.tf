e8ae2b616a5b613f6637d58882d8d19264b00456
TaiBT2 <thanhtai18021994@gmail.com>
2023-05-23 13:27:51 +0700

    jekins

--- devops-tool/infra/main.tf
+++ devops-tool/infra/main.tf
@@ -39,15 +39,20 @@ resource "aws_instance" "server" {
   tags = {
     project = "${var.name_project}"
   }
+  
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update",
-      "sudo apt install apt-transport-https ca-certificates curl software-properties-common",
-      "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-      "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-      "sudo apt update",
-      "apt-cache policy docker-ce",
-      "sudo apt install -y docker-ce"
+      # "sudo apt update",
+      # "sudo apt install apt-transport-https ca-certificates curl software-properties-common",
+      # "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+      # "echo 'deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      # "sudo apt update",
+      # "apt-cache policy docker-ce",
+      # "sudo apt install -y docker-ce"
+      "sudo apt update -y",
+      "sudo apt install -y software-properties-common",
+      "sudo apt-add-repository --yes --update ppa:ansible/ansible",
+      "sudo apt install -y ansible"
     ]
 
     connection {
